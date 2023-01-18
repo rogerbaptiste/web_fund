b0e85d715bac37db4d6d65efd4dbfb38fe9eca44
Roger Baptiste <rogerbap@outlook.com>
2023-01-17 22:39:13 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+    function login(element) {
+        if (element.innerText == "Login"){
+            element.innerText = "logout"
+        }else{
+            element.innerText == "Login"
+        }
+    }
+
+    function add_definition(element) {
+        element.remove();
+    }
